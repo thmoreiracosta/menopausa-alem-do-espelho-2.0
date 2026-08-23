062a77fde3aed855bdef6875a621ac1ee108c8b5
Thiago Costa <thiagomoreiracosta@gmail.com>
2026-08-23 12:57:40 -0300

    configura deploy automatico no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  base: "/menopausa-alem-do-espelho-2.0/",
+
   plugins: [
     react(),
     TanStackRouterVite(),
